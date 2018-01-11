6060e941e656efeadfaaceb999578e7ebec4c59a
Sanat Sharma <sanatcolumban@gmail.com>
2018-01-12 03:05:48 +0530

    Add ability to handle multiple sessions to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3000 });
 console.log("Here");
+var teacher_conn = {};
+var teachers = {}
 
 wss.on('connection', function connection(ws, req) {
   const ip = req.connection.remoteAddress;
@@ -12,17 +14,69 @@ wss.on('connection', function connection(ws, req) {
 
   ws.on("close", function(){
     console.log("removing client " + ws);
+
+    if (ws in teachers){
+      console.log("Teacher died, kill all center connections");
+      // if teacher connection dies then kill all of its corresponding student connections also
+      teacher_conn[teachers[ws]].forEach(function each(client){
+        client.close();
+      });
+
+      // delete enrty from teachers dict
+      delete teachers[ws];
+      console.log("Teachers: " + JSON.stringify(teachers));
+    }
+
     wss.clients.delete(ws);
+  
   });
 
   ws.on("message", function ws_message(data){
-    console.log("Data: " + data);
-    // Broadcast message to all the centers
-    wss.clients.forEach(function each(client){
-        if (client !== ws && client.readyState == WebSocket.OPEN){
-          client.send(data);
+
+    if (isJson(data)){
+      // passed data is JSON
+      var obj = JSON.parse(data);
+      if ('type' in obj){
+        if (obj.type == "teacher"){
+          console.log("Teacher Connection")
+          teachers[ws] = obj.session;
+          teacher_conn[obj.session] = [];
+          console.log(teacher_conn)
         }
-    });
+        else if (obj.type == "center"){
+          console.log("Center Connection");
+
+          // check if the session the center is trying to log
+          if (obj.session in teacher_conn){
+            teacher_conn[obj.session].push(ws);
+            console.log(teacher_conn);
+          }
+          else{
+            console.log("No corresponding session");
+            ws.close();
+          }
+        }
+      }
+      else{
+        console.log("Data: " + obj.data);
+        // Broadcast message to all the centers
+        var d = obj.data;
+        if (obj.session in teacher_conn){
+          teacher_conn[obj.session].forEach(function each(client){
+            client.send(d)
+          });
+        }
+        else{
+          console.log("No corresponding session");
+          ws.close();
+        }
+
+      }
+    }
+    else{
+      console.log("Data not JSON!");
+      ws.close();
+    }
   });
   // Handle any errors that occur.
   ws.on("error", function(error) {
@@ -30,3 +84,14 @@ wss.on('connection', function connection(ws, req) {
   });
 });
 
+
+function isJson(str) {
+  try {
+      console.log(str);
+      JSON.parse(str);
+  } catch (e) {
+      console.log(e);
+      return false;
+  }
+  return true;
+}
