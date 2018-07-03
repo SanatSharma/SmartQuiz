c16632ac5612d01a5802a72f57ce3f5df0f69899
Sanat Sharma <sanatcolumban@gmail.com>
2018-07-03 00:57:12 -0700

    Small rest change

--- WebSocketClient/Rest.cpp
+++ WebSocketClient/Rest.cpp
@@ -18,13 +18,23 @@ int Rest::getSession(std::string macAddr, std::string streamKey)
 
 int Rest::getRRQ(int session)
 {
-	std::string rrqUrl = base_url + "/" + std::to_string(session) + "/getRRQ";
+	std::string rrqUrl = base_url + std::to_string(session) + "/getRRQ";
 
 	uri* url = new uri(Utilities::convertToWString(rrqUrl).c_str());
 	std::string val = Utilities::HTTPStreamingAsync(url).get();
 
 	std::cout << val << std::endl;
 
+
 	json j = json::parse(val);
 	return int(j["RRQ_ID"]);
+}
+
+//("api/{sessionId:int}/{rrqId:int}/saveRRQResponse/{QId:int}/{remoteId}/{response}")
+void Rest::postResponse(int remoteId, char * data)
+{
+	std::string posturl = base_url + std::to_string(SESSION) + "/saveRRQResponse/" + std::to_string(QID) + "/" + std::to_string(remoteId) + "/" + data;
+
+	uri* url = new uri(Utilities::convertToWString(posturl).c_str());
+	std::string val = Utilities::HTTPStreamingAsync(url).get();
 }
