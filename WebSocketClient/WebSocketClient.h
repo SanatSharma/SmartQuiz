08093d5cfd6eda8071ebc2e903e77ec8fb427ca2
Sanat Sharma <sanatcolumban@gmail.com>
2018-07-09 00:43:53 -0700

    Qid change works

--- WebSocketClient/WebSocketClient.h
+++ WebSocketClient/WebSocketClient.h
@@ -1,22 +1,12 @@
-#include <uWS/uWS.h>
+#pragma once
+
 #include "Quiz.h"
-#include <agents.h>
-#include <ppltasks.h>
 #include "Sql.h"
 #include "Rest.h"
 #include "Utilities.h"
 #include <nlohmann/json.hpp>
 
-Concurrency::call<int> *call;
-Concurrency::timer<int> *timer;
-const char* START = "start";
-const char* NEXT = "next";
-const char* STOP = "stop";
-const char* ATTENDANCE = "attendance";
-const char* STOPATTENDANCE = "stopattendance";
-const char* QIDNUM = "Qid";
-int attendance_condition = 0;
-int SESSION;
-int QID;
-int RRQ_ID = -1;
+extern int SESSION;
+extern int QID;
+extern int RRQ_ID;
 using json = nlohmann::json;
